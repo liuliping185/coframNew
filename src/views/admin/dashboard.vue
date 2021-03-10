27b283ee8714065fe5113a23beb52bded4faf3f1
liuliping <18535741140@163.com>
2021-03-10 18:01:13 +0800

    初始化提交

--- src/views/admin/dashboard.vue
+++ src/views/admin/dashboard.vue
@@ -2,10 +2,8 @@
   <div class="p-20">
     <el-alert
       center
-      :closable="false"
-    >
+      :closable="false">
       <div class="f-22">{{$t('route.dashboarddisplayinfo')}}</div>
-
     </el-alert>
   </div>
 </template>
